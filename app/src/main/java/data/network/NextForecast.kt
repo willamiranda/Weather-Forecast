e584fbc1ec94adeaee57f52089fa555ba8b88fef
Wiilames <willmiranda.94@gmail.com>
2022-08-29 08:52:21 -0400

    change navigation to app works and bug's fixed

--- app/src/main/java/data/network/NextForecast.kt
+++ app/src/main/java/data/network/NextForecast.kt
@@ -5,23 +5,16 @@ import com.google.gson.annotations.SerializedName
 
 data class CurrentWeatherEntry(
 
-    @SerializedName("feelslike_c")
-    val feelslike_c: Float,
-
-    @SerializedName("precip_mm")
-    val precip_mm: Float,
 
     @SerializedName("temp_c")
     val temp_c: Float,
 
-    @SerializedName("vis_km")
-    val vis_km: Int,
+
 
     @SerializedName("condition")
     val condition: Condition,
 
-    @SerializedName("wind_mph")
-    val wind_mph: Float
+
 
 )
 
